a9c02f2fbfe19f75fe807fdf7d10406f4c515875
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2019-12-02 13:49:04 +0100

    comletion du template d'application de base

--- lib/controllers/node.controller.ts
+++ lib/controllers/node.controller.ts
@@ -3,9 +3,9 @@ import { Request, Response } from "express";
 export class NodesController {
 	public index(req: Request, res: Response) {
 		let id = req.params.id;
-		console.log(id);
 		res.json({
-			message: "Hello boi"
+			message: "Hello boi",
+			input_id: id
 		});
 	}
 }
