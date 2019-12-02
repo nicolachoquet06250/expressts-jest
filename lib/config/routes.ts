5349f5297de19517096fc5affb5f1efcb24b83e8
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2019-12-02 14:11:21 +0100

    add nodemon for typescript

--- lib/config/routes.ts
+++ lib/config/routes.ts
@@ -5,8 +5,10 @@ export class Routes {
 	public nodesController: NodesController = new NodesController();
 
 	public routes(app): void {
-		app.route("/:id").get(this.nodesController.index);
+		app.route("/").get(this.nodesController.index);
 
 		app.route("/nodes").get(this.nodesController.index);
+
+		app.route("/:id").get(this.nodesController.index);
 	}
 }
--- lib/controllers/node.controller.ts
+++ lib/controllers/node.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export class NodesController {
 	public index(req: Request, res: Response) {
-		let id = req.params.id;
+		let id: number = req.params.id !== undefined ? parseInt(req.params.id) : 0;
 		res.json({
 			message: "Hello boi",
 			input_id: id
